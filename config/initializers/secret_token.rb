77da59a2f4140e0fd350531133b9f773dfdcc53f
Tony612 <h.bing612@gmail.com>
2012-10-31 23:15:32 +0800

    Init the project and modify the gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tony612Com::Application.config.secret_token = 'ccdfad4f6d7673909b0a84385a4ed710dc42477871fa95749a68f7504f0dd321cbfea0abdd4798aa9ecfddf10a558ec5937b67f345e9ff11824a60799327c52c'
