c42d9a669cd607a00f5cd7c61bb6a36c3f7b250c
Tony612 <h.bing612@gmail.com>
2012-11-01 15:38:20 +0800

    Fix the bug of code's symbol show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  
   def markdown(text)
     coderayified = HTMLwithCodeRay.new(:filter_html => true, :hard_wrap => true)
     options = {   
@@ -7,15 +8,16 @@ module ApplicationHelper
       :fenced_code_blocks => true,
       :no_intra_emphasis => true,
       :strikethrough =>true,
-      :lax_html_blocks => true,
-      :superscript => true
+      #:lax_html_blocks => true,
+      #:superscript => true
     }
     #markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,options)
     markdown = Redcarpet::Markdown.new(coderayified,options)
-    markdown.render(h(text)).html_safe
+    markdown.render(text).html_safe
   end
 
   class HTMLwithCodeRay < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
     def block_code(code, language)
       CodeRay.scan(code, language).div(:tab_width=>2, :line_numbers => :table)
     end
