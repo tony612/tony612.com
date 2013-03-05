2046999b683e7df5de15a21643f67269a5fce04f
tony612 <h.bing612@gmail.com>
2013-03-05 11:26:33 +0800

    make admin and post use the strong parameters

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,4 @@
 class AdminsController < ApplicationController
   def show
-    
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :created_at, :category
+  #attr_accessible :content, :title, :created_at, :category
 
   validates_presence_of :title, :content
 
