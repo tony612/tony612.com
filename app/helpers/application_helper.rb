f4a792cddb081f46b7780a3c0deb70fc43e24059
tony612 <h.bing612@gmail.com>
2013-08-17 13:04:39 +0800

    make the cache_key_for_posts more accurate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def cache_key_for_posts(posts)
-    max_id         = posts.maximum(:id).try(:to_s, :number)
-    max_updated_at = posts.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    max_id         = posts.select(:id).max.id
+    max_updated_at = posts.select(:updated_at).max.updated_at.try(:to_s, :number)
     "posts/all-#{max_id}-#{max_updated_at}"
   end
 end
