2467403e1769bcd92d7c827fc9b20b10f2734ce1
tony612 <h.bing612@gmail.com>
2013-08-17 10:34:51 +0800

    define the cache_key_for_posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def cache_key_for_posts(posts)
+    max_id         = posts.maximum(:id).try(:to_s, :number)
+    max_updated_at = posts.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "posts/all-#{max_id}-#{max_updated_at}"
+  end
 end
