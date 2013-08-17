3016574f2f60c6e518d6ffeae7bf90ab13fb3198
tony612 <h.bing612@gmail.com>
2013-08-17 10:33:33 +0800

    remove the warnings by change scope to lambda

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,10 @@
 class Post < ActiveRecord::Base
-  #attr_accessible :content, :title, :created_at, :category, :markdown_html, :excerpt_markdown
 
   validates_presence_of :title, :content
 
-  scope :order_by_time, order("created_at DESC")
-  scope :life, where(category: "life").order_by_time
-  scope :tech, where(category: "tech").order_by_time
+  scope :order_by_time, -> { order("created_at DESC") }
+  scope :life, -> { where(category: "life").order_by_time }
+  scope :tech, -> { where(category: "tech").order_by_time }
 
   include Markdown
 
