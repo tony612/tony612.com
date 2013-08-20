e30e36f82f4e9ec99a86ce98434c0cd12c06dcfb
tony612 <h.bing612@gmail.com>
2013-08-20 19:13:30 +0800

    pass posts directly as parameter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def cache_key_for_posts(posts)
-    max_id         = posts.max_by(&:id).id
-    max_updated_at = posts.max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
-    "posts/all-#{max_id}-#{max_updated_at}"
-  end
 end
